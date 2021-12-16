57be86a62ba179a1b5bd363e3a5e26b81f70881d
Jeano Frederick Ermitaño <jeano.ermitano@laspinas.sti.edu.ph>
2021-12-16 11:54:10 +0800

    added coin picker

--- PreFinalProject/PreFinalProject/WelcomePage.xaml.cs
+++ PreFinalProject/PreFinalProject/WelcomePage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Net.Http;
 using System.Text;
@@ -17,6 +18,8 @@ namespace PreFinalProject
 
         public HttpClient client = new HttpClient();
         public string endpoint = "https://api.coingecko.com/api/v3/";
+        public string selectedCoin;
+        public string selectedCurrency;
         public WelcomePage()
         {
             InitializeComponent();
@@ -35,45 +38,42 @@ namespace PreFinalProject
             string response = await client.GetStringAsync(endpoint + "ping");
             lblResponse.Text = response;
 
-
+            string coinResponse = await client.GetStringAsync(endpoint + "coins/list");
+            List<Coins> deserializedCoinResponse = JsonConvert.DeserializeObject<List<Coins>>(coinResponse);
+            foreach (Coins c in deserializedCoinResponse)
+            {
+                pickerCoin.Items.Add(c.id);
+            }
 
             string currencyResponse = await client.GetStringAsync(endpoint + "simple/supported_vs_currencies");
             List<string> deserializedCurrencyResponse = JsonConvert.DeserializeObject<List<string>>(currencyResponse);
             pickerCurrency.ItemsSource = deserializedCurrencyResponse;
         }
 
-        /*
-        private async void btnBitcoinBuyPrice_Clicked(object sender, EventArgs e)
-        {
-            string response = await client.GetStringAsync(endpoint + "simple/price?ids=bitcoin&vs_currencies=php");
-            lblResponse.Text = response;
-        }
-
-        private async void btnSLPBuyPrice_Clicked(object sender, EventArgs e)
-        {
-            string response = await client.GetStringAsync(endpoint + "simple/price?ids=smooth-love-potion&vs_currencies=php");
-            lblResponse.Text = response;
-        }
-
-        private async void btnShibaInuBuyPrice_Clicked(object sender, EventArgs e)
-        {
-            string response = await client.GetStringAsync(endpoint + "simple/price?ids=shiba-inu&vs_currencies=php");
-            lblResponse.Text = response;
-        }
-        */
-
         private void pickerCurrency_SelectedIndexChanged(object sender, EventArgs e)
         {
-
+            selectedCurrency = pickerCurrency.SelectedItem.ToString();
         }
 
         private void pickerCoin_SelectedIndexChanged(object sender, EventArgs e)
         {
-
+            selectedCoin = pickerCoin.SelectedItem.ToString();
         }
 
-        private void btnCheckPrice_Clicked(object sender, EventArgs e)
+        private async void btnCheckPrice_Clicked(object sender, EventArgs e)
         {
+            string response = await client.GetStringAsync(endpoint + "simple/price?ids=" + selectedCoin + "&vs_currencies=" + selectedCurrency);
+            // response format is { coin : { currency : price } }
+            // example: { "bitcoin" : { "usd" : 48910 } }
+            // we use dictionary to model the same structure of key-value pairs
+            Dictionary<string, Dictionary<string, int>> deserializedResponse = JsonConvert.DeserializeObject<Dictionary<string, Dictionary<string, int>>>(response);
+
+
+            Dictionary<string, int> currencyPairPrice;
+            bool a = deserializedResponse.TryGetValue(selectedCoin, out currencyPairPrice);
+            int price;
+            bool b = currencyPairPrice.TryGetValue(selectedCurrency, out price);
+            lblResponse.Text = "The price is " + price;
 
         }
     }
